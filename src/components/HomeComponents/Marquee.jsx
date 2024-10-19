153f6fccbf46b816622b685138b0de0bb3eee446
Deepak ES <deepakes2003@gmail.com>
2024-10-19 12:07:59 +0530

    updated home section

--- src/components/HomeComponents/Marquee.jsx
+++ src/components/HomeComponents/Marquee.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import confetti from "../../assets/confetti.gif";
-import './Marquee.css'; // Assuming you create this CSS file
+
 
 function Marquee() {
   return (
     <div className="marquee-container">
       <div className="marquee">
         <div className="flex gap-10 md:gap-4 items-center">
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-2 p-3 items-center">
             <h1 className='text-xl font-semibold'>Announcement</h1>
             <img src={confetti} className="h-[30px] w-[30px]" alt="confetti" />
           </div>
